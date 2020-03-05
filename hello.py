002c1ed5d3b5e190e44eac5e5ef3f1b9cafb155f
= <gyric.trinidad@mnltechnology.com>
2020-03-05 00:09:26 -0800

    Adding .gitignore and test

--- hello.py
+++ hello.py
@@ -8,8 +8,8 @@ def hello():
 
 @app.route("/hello/<string:name>/")
 def yo(name):
-    quotes = [ "'If people do not believe that mathematics is simple, it is only because they do not realize how complicated life is.' -- John Louis von Neumann ",
-    "'Computer science is no more about computers than astronomy is about telescopes' --  Edsger Dijkstra ",
+    quotes = [ "'Yeah yeah yeah yeah yeah' -- John Louis von Neumann ",
+    "'Go go go go' --  Edsger Dijkstra ",
     "'To understand recursion you must first understand recursion..' -- Unknown",
     "'You look at things that are and ask, why? I dream of things that never were and ask, why not?' -- Unknown",
     "'Mathematics is the key and door to the sciences.' -- Galileo Galilei",
